e3fe3fa9126742d55ff9ac1ec39094cd39c9e393
Deltorean <37897326+Deltorean@users.noreply.github.com>
2019-05-08 17:29:15 +0200

    animationadd

---
+++ Assets/Scripts/BobController.cs
@@ -0,0 +1,50 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class BobController : MonoBehaviour
+{
+
+    public Animator anim;
+    public float speed;
+    public float direction;
+    private object rb;
+
+    int jumpHash = Animator.StringToHash("Jump");
+    int runStateHash = Animator.StringToHash("Base Layer.Run");
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        speed = Input.GetAxis("Vertical");
+        direction = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("speed", speed);
+        anim.SetFloat("direction", direction);
+
+        if (Input.GetButtonDown("Fire1"))
+            anim.SetTrigger("taunt");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            anim.SetTrigger("jump");
+
+            AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+            if (Input.GetKeyDown(KeyCode.Space) && stateInfo.nameHash == runStateHash)
+            {
+                anim.SetTrigger(jumpHash);
+            }
+
+        }
+}
+}
--- Assets/Scripts/CharacterStates/GroundedState.cs
+++ Assets/Scripts/CharacterStates/GroundedState.cs
@@ -5,15 +5,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 [CreateAssetMenu(menuName = "Character/GroundedState")]
 public class GroundedState : CharacterBaseState
 {
+
+    public Animator anim;
+    public float speed;
+    public float direction;
+    private object rb;
+    int jumpHash = Animator.StringToHash("Jump");
+    int runStateHash = Animator.StringToHash("Base Layer.Run");
+
+
     public override void EnterState()
     {
         base.EnterState();
 
         dynamicFriction = 0.35f;
         MaxSpeed = 10;
+
+        anim = owner.GetComponent<Animator>();
     }
 
     public override void ToDo()
@@ -33,10 +45,21 @@ public class GroundedState : CharacterBaseState
         #endregion
         ChangeCharRotation();
         Gravity();
+
+        Speed();
+
+
         #region Buttons
         if (Input.GetKeyDown(KeyCode.Space))
         {
             ApplyForce(new Vector3(0, jumpHeight, 0));
+            anim.SetTrigger("jump");
+
+            AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+            if (Input.GetKeyDown(KeyCode.Space) && stateInfo.nameHash == runStateHash)
+            {
+                anim.SetTrigger(jumpHash);
+            }
         }
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
@@ -74,5 +97,19 @@ public class GroundedState : CharacterBaseState
 
     }
 
+    void Speed()
+    {
+        speed = Input.GetAxis("Vertical");
+        direction = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("speed", speed);
+        anim.SetFloat("direction", direction);
+
+
+
+
+
+    }
 
 }
+
