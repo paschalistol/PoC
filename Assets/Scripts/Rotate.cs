fda9e07acaba28fd8dad0069c549994d315f8cb4
mikoqbaker <37655955+mikoqbaker@users.noreply.github.com>
2019-05-25 13:21:11 +0200

    platrorming

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+
+
+
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, 1f, 0f));
+        
+    }
+}
