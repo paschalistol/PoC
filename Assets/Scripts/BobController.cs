9d0faa58a418b08d9778109897af9487c63a3630
TeaLund <tea.lundstrom@gmail.com>
2019-05-28 12:55:33 +0200

    all animations in place

--- Assets/Scripts/BobController.cs
+++ Assets/Scripts/BobController.cs
@@ -20,14 +20,25 @@ public class BobController : MonoBehaviour
     {
         speed = Input.GetAxis("Vertical");
         direction = Input.GetAxis("Horizontal");
+        anim.SetFloat("Direction", direction);
 
-        anim.SetFloat("Speed", speed);
-        anim.SetFloat("Direction", direction);      
+        if (!Input.GetKey(KeyCode.LeftShift))
+        {
+            anim.SetFloat("Speed", speed);            
+        }
+        else if (Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.S))
+        {
+            anim.SetFloat("Speed", 2f);
+        }
+        else if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.S))
+        {
+            anim.SetFloat("Speed", -2f);
+        }
+              
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            anim.SetTrigger("Jump");     
-
+            anim.SetTrigger("Jump");            
         }
 
         if(player.GetComponent<CharacterStateMachine>().grounded == true)
@@ -35,6 +46,9 @@ public class BobController : MonoBehaviour
             anim.SetBool("Grounded", true);
         }
 
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            anim.SetTrigger("Pickup");
+        }             
     }
 }
