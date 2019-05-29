d0f18f5606a7e9d48bf9956c06307a8b87d7eb6a
paschalisTol <37561700+paschalistol@users.noreply.github.com>
2019-05-29 10:42:02 +0200

    press e to interact

---
+++ Assets/Scripts/EventSystem/Listeners/PressEListener.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Callback
+{
+    public class PressEListener : MonoBehaviour
+    {
+        [SerializeField] private GameObject text;
+        void Start()
+        {
+            EventSystem.Current.RegisterListener<PressE>(ToggleText);
+        }
+
+        void ToggleText(PressE info)
+        { 
+            text.SetActive(info.open);
+        }
+    }
+}
