1708e813990892914482235ca0ce0c2bbfea8a8f
mikoqbaker <37655955+mikoqbaker@users.noreply.github.com>
2019-05-31 00:37:21 +0200

    wdwdw

---
+++ Assets/Scripts/CameraPan.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPan : MonoBehaviour
+{
+
+
+
+
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, 0.5f, 0f));
+        
+    }
+}
