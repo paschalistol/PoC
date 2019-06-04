2bd28175a0e3ac85fae6ba2e696d5775d4f31c46
mikoqbaker <37655955+mikoqbaker@users.noreply.github.com>
2019-06-04 22:31:53 +0200

    controls

---
+++ Assets/Controls.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DisableControls());   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator DisableControls()
+    {
+        yield return new WaitForSeconds(30);
+        gameObject.SetActive(false);
+    }
+}
